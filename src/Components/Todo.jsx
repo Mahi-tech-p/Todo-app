47df1b9f3c1208e21532f0784ebe1182bce1dfea
Mahi-tech-p <leocodes01@gmail.com>
2024-10-03 17:52:37 +0530

    changes in button

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -5,7 +5,7 @@ function Todo() {
 
     const inputRef = useRef()
 
-    const [todoList, setTodoList] = useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")): []);
+    const [todoList, setTodoList] = useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")):[]);
 
     const add = () => {
         const inputText = inputRef.current.value.trim();
@@ -54,6 +54,7 @@ function Todo() {
             <div className='flex items-center my-7 bg-gray-200 rounded-full'>
                 <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' placeholder='Add your task ' />
                 <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer' onClick={add}>ADD +</button>
+                <button>delete</button>
             </div>
             {/* todo-list */}
             <div>
