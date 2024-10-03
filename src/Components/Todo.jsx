156e12d846ac8a0a575ee8fb38d458e18f7e3e20
Mahi-tech-p <leocodes01@gmail.com>
2024-10-03 17:57:27 +0530

    revert in changes in button

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -54,7 +54,6 @@ function Todo() {
             <div className='flex items-center my-7 bg-gray-200 rounded-full'>
                 <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' placeholder='Add your task ' />
                 <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer' onClick={add}>ADD +</button>
-                <button>delete</button>
             </div>
             {/* todo-list */}
             <div>
